41a6b067f7b25fddbb9c98ed2adf8211e8547904
Pedro Barão <phbarao@gmail.com>
2021-11-11 17:58:52 -0300

    refactor: group imports in index files

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,8 +1,7 @@
-import { HttpResponse, HttpStatusCode } from '@/data/protocols/http/http-response'
 import {
-  HttpPostClient,
+  HttpResponse, HttpStatusCode, HttpPostClient,
   HttpPostParams
-} from '@/data/protocols/http/http-post-client'
+} from '@/data/protocols/http'
 
 export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
   url?: string
--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,13 +1,12 @@
 import faker from 'faker'
 
-import { HttpPostClientSpy } from '@/data/test/mock-http-client'
-import { HttpStatusCode } from '@/data/protocols/http/http-response'
-import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
-import { UnexpectedError } from '@/domain/errors/unexpected-error'
-import { mockAuthentication, mockAccountModel } from '@/domain/test/mock-account'
 import { RemoteAuthentication } from './remote-authentication'
-import { AuthenticationParams } from '@/domain/usecases/authentication'
-import { AccountModel } from '@/domain/models/account-model'
+import { HttpPostClientSpy } from '@/data/test'
+import { HttpStatusCode } from '@/data/protocols/http'
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
+import { mockAuthentication, mockAccountModel } from '@/domain/test'
+import { AuthenticationParams } from '@/domain/usecases'
+import { AccountModel } from '@/domain/models'
 
 interface SutTypes {
   sut: RemoteAuthentication
